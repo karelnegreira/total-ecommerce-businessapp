19240b40303aaecfae0699ff2bca8686b795cf07
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-10 02:14:53 +1200

    command and popover library added from shadcn

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -25,7 +25,8 @@ export function MainNav({className, ...props}: React.HTMLAttributes<HTMLElement>
         >   
 
         {routes.map((route) => (
-            <Link key={route.href} href={route.href} className={cn("text-sm font-medium transition-colors hover:text-primary")}>
+            <Link key={route.href} href={route.href} className={cn("text-sm font-medium transition-colors hover:text-primary", 
+                    route.active ? "text-black dark:text-white": "text-muted-foreground")}>
                 {route.label}
             </Link>
         ))}
