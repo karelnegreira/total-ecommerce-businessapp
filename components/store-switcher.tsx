eea987a3b8d644bca9e0ed4d30e68a26aa0e7a49
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-10 02:57:08 +1200

    store-switcher onstoreselect val is declared / on select store redirects it to it

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -3,6 +3,8 @@
 import { PopoverTrigger } from "@/components/ui/popover";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Store } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -12,7 +14,23 @@ interface StoreSwitcherProps extends PopoverTriggerProps {
 
 export default function StoreSwitcher({className, items = []}: StoreSwitcherProps) {
     const storeModal = useStoreModal();
+    const params = useParams();
+    const router = useRouter();
     
+    const formattedItems = items.map((item) => ({
+        label: item.name, 
+        value: item.id
+    }));
+
+    const currentStore = formattedItems.find((item) => item.value === params.storeId);
+
+    const [open, setOpen] = useState(false);
+
+    const onStoreSelect = (store: {value: string, label: string}) => {
+        setOpen(false);
+        router.push(`/${store.value}`);
+    }
+
     return (
         <div>
             Store Switcher
