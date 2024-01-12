c3f0321d1e19177a83f068dd310fbbbbbb6f66af
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-13 02:04:34 +1200

    chevron icon added in store-switcher component

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { Popover, PopoverTrigger } from "@/components/ui/popover";
-import { useStoreModal } from "@/hooks/use-store-modal";
+import { ChevronsUpDown, Store as StoreIcon } from "lucide-react";
 import { Store } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
-import { Button } from "./ui/button";
-import { Store as StoreIcon } from "lucide-react";
+
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -36,8 +38,11 @@ export default function StoreSwitcher({className, items = []}: StoreSwitcherProp
     return (
         <Popover open={open} onOpenChange={setOpen}>
            <PopoverTrigger asChild>
-               <Button>
-                   <StoreIcon />
+               <Button variant="outline" size="sm" role="combobox" aria-expanded={open} aria-label="Select a store" 
+               className={cn("w-[200px] justify-between", className)}>
+                   <StoreIcon className="mr-2 h-4 w-4"/>
+                   Current Store
+                   <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50"/>
                 </Button> 
            </PopoverTrigger>
         </Popover>
