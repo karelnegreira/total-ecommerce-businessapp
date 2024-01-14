ef93b58e49eb8ca75437a42ef42020ddbc0740a4
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-15 04:38:46 +1200

    Trash icon added into SettingsForm component

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
 
 interface StoreSettingsProps {
     initialData: Store;
@@ -12,6 +14,9 @@ const SettingsForm: React.FC<StoreSettingsProps> = ({initialData}) => {
   return (
     <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="sm" onClick={() => {}}>
+            <Trash className="h-4 w-4"/>
+        </Button>
     </div>
   )
 }
