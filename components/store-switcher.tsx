4f6d25035cfa51370c4b403c13920f5b0ca00289
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-14 13:00:20 +1200

    active store label passed as props to the store-switcher component

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -42,7 +42,7 @@ export default function StoreSwitcher({className, items = []}: StoreSwitcherProp
                <Button variant="outline" size="sm" role="combobox" aria-expanded={open} aria-label="Select a store" 
                className={cn("w-[200px] justify-between", className)}>
                    <StoreIcon className="mr-2 h-4 w-4"/>
-                        Current Store
+                        {currentStore?.label}
                    <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50"/>
                 </Button> 
            </PopoverTrigger>
