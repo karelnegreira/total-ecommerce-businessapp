2c395960dcfd48a1434747c70e3d4bde6e3d4881
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-15 05:14:52 +1200

    formSchema const created

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,23 +1,32 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import Heading from "@/components/ui/heading";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import  * as z  from "zod";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 
 interface StoreSettingsProps {
     initialData: Store;
 }
 
+const formSchema = z.object({
+    name: z.string().min(1);
+});
 
 const SettingsForm: React.FC<StoreSettingsProps> = ({initialData}) => {
   return (
-    <div className="flex items-center justify-between">
-        <Heading title="Settings" description="Manage store preferences" />
-        <Button variant="destructive" size="sm" onClick={() => {}}>
-            <Trash className="h-4 w-4"/>
-        </Button>
-    </div>
+    <>
+        <div className="flex items-center justify-between">
+            <Heading title="Settings" description="Manage store preferences" />
+            <Button variant="destructive" size="sm" onClick={() => {}}>
+                <Trash className="h-4 w-4"/>
+            </Button>
+        </div>
+        <Separator />
+    </>
   )
 }
 
