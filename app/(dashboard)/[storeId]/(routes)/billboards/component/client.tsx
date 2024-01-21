b9f47d0b714af6f3837e64863a01171e336b7d1d
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-22 07:45:45 +1200

    router and params added in button, no page found error yet it redirects

--- app/(dashboard)/[storeId]/(routes)/billboards/component/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/component/client.tsx
@@ -6,10 +6,14 @@ import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
 
 
 
 export const BillboardClient = () => {
+    const router = useRouter();
+    const params = useParams();
+
   return (
     <>
         <div className="flex items-center justify-between">
@@ -17,14 +21,14 @@ export const BillboardClient = () => {
                 title="Billboards (0)"
                 description="Manage the billboards of your store"
             />
-            <Button>
+            <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                 <Plus className="mr-2 h-4 w-4" />
                 Add New
             </Button>
         </div>
         <Separator />
     </>
-    
+
   )
 }
 
