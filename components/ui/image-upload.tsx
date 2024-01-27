d11efdac7612feb7d969f6cd390ab8f15e558b8b
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-01-27 22:33:21 +1200

    ImageUpload component being created

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+    disabled: boolean;
+    onChange: (value: string) => void;
+    onRemove: (value: string) => void;
+    value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({disabled, onChange, onRemove, value}) => {
+    
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <div>ImageUpload</div>
+    )
+}
+
+export default ImageUpload;
