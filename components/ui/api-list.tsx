12bc5a7d28b1ed795e79e9d3c0ff1c4d99843668
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-02-11 10:35:11 +1200

    API string shown at the botton at client component.

--- components/ui/api-list.tsx
+++ components/ui/api-list.tsx
@@ -23,6 +23,26 @@ export const ApiList: React.FC<ApiListProps> = ({entityName, entityIdName}) => {
                 variant="public"
                 description={`${baseUrl}/${entityName}`}
             />
+            <ApiAlert 
+                title="GET"
+                variant="public"
+                description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+            />
+            <ApiAlert 
+                title="POST"
+                variant="admin"
+                description={`${baseUrl}/${entityName}`}
+            />
+            <ApiAlert 
+                title="PATCH"
+                variant="admin"
+                description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+            />
+            <ApiAlert 
+                title="DELETE"
+                variant="admin"
+                description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+            />
         </>
     );
 }
