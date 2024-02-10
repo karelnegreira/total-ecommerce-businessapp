772e1bec66640f5dc0c112f4d85b4b6afa7c5114
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-02-11 00:12:46 +1200

    delete billboards functionality is made

--- app/(dashboard)/[storeId]/(routes)/billboards/component/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/component/cell-action.tsx
@@ -10,6 +10,7 @@ import toast from "react-hot-toast";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 
 interface CellActionProps {
@@ -31,9 +32,8 @@ export const CellAction: React.FC<CellActionProps> = ({data,}) => {
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
+            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
             router.refresh();
-            router.push("/");
             toast.success("Billboard deleted.");
         } catch (error) {
             toast.error("Make sure you remove all categories using this billboard")
@@ -44,6 +44,10 @@ export const CellAction: React.FC<CellActionProps> = ({data,}) => {
     };
 
   return (
+    <>
+    
+    <AlertModal isOpen={open} onClose={() => setOpen(false)} onConfirm={onDelete} loading={loading} />
+    
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="h-8 w-8 p-0">
@@ -63,11 +67,12 @@ export const CellAction: React.FC<CellActionProps> = ({data,}) => {
                 <Edit className="mr-2 h-4 w-4"/>
                     Update
             </DropdownMenuItem>
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setOpen(true)}>
                 <Trash className="mr-2 h-4 w-4"/>
                     Delete
             </DropdownMenuItem>
         </DropdownMenuContent>
     </DropdownMenu>
+    </>
   );
 };
