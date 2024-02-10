b995ac1eb9d728f5c678f492d1ccfc8410ccfa5d
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-02-11 00:20:15 +1200

    billboard-form component onDelete function now redirects to billboards list once clicked

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -76,7 +76,7 @@ const BillboardForm: React.FC<BillboardFormProps> = ({initialData}) => {
             setLoading(true);
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
             router.refresh();
-            router.push("/");
+            router.push(`/${params.storeId}/billboards`);
             toast.success("Billboard deleted.");
         } catch (error) {
             toast.error("Make sure you remove all categories using this billboard")
