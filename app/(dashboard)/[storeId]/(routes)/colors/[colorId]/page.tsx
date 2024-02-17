67dac8c17961fa8cd3420c331f154d03783c1e04
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-02-18 04:27:42 +1200

    ColorPage component updated

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -2,20 +2,20 @@ import prismadb from "@/lib/prismadb";
 import SizeForm from "./components/size-form";
 
 
-const SizePage = async ({params} : {params: {sizeId: string}}) => {
-    const size = await prismadb.size.findUnique({
+const ColorPage = async ({params} : {params: {colorId: string}}) => {
+    const color = await prismadb.color.findUnique({
         where: {
-            id: params.sizeId
+            id: params.colorId
         }
     });
 
   return (
     <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-          <SizeForm initialData={size} />
+          <SizeForm initialData={color} />
         </div>
     </div>
   )
 }
 
-export default SizePage;
+export default ColorPage;
