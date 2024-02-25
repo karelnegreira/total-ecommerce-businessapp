95b753922719797f9734c94fe5bd1354d33aefdb
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-02-25 19:17:42 +1200

    cell-actions.tsx / product updated and searchKey replaced to names / bug fixed

--- app/(dashboard)/[storeId]/(routes)/products/component/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/component/cell-action.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 
-import { Button } from "@/components/ui/button";
-import { ProductColumn } from "./columns";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import toast from "react-hot-toast";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import axios from "axios";
-import { AlertModal } from "@/components/modals/alert-modal";
 
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { AlertModal } from "@/components/modals/alert-modal";
+import { Button } from "@/components/ui/button";
+import { ProductColumn } from "./columns";
 
 interface CellActionProps {
     data: ProductColumn;
@@ -26,17 +26,17 @@ export const CellAction: React.FC<CellActionProps> = ({data,}) => {
 
     const onCopy = (id: string) => {
         navigator.clipboard.writeText(id);
-        toast.success("Billboard id copied to the clipboard");
+        toast.success("Product id copied to the clipboard");
     };
 
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+            await axios.delete(`/api/${params.storeId}/products/${data.id}`)
             router.refresh();
-            toast.success("Billboard deleted.");
+            toast.success("Product deleted.");
         } catch (error) {
-            toast.error("Make sure you remove all categories using this billboard")
+            toast.error("Something went wrong")
         } finally {
             setLoading(false);
             setOpen(false);
@@ -63,7 +63,7 @@ export const CellAction: React.FC<CellActionProps> = ({data,}) => {
                 <Copy className="mr-2 h-4 w-4"/>
                     Copy Id
             </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}>
+            <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/products/${data.id}`)}>
                 <Edit className="mr-2 h-4 w-4"/>
                     Update
             </DropdownMenuItem>
