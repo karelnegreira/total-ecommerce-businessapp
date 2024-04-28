3481153785a8bb854f4800a43a7fa6fedf635980
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-04-28 21:51:01 +1200

    card componen installed

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -14,6 +14,9 @@ const DashboardPage: React.FC<DashboadPageProps> = async ({params}) => {
         <div className="flex-1 space-y-4 p-8 pt-6">
           <Heading title="Dashboard" description="Overview your store"/>
           <Separator/>
+          <div className="grid gap-4 grid-cols-3">
+            
+          </div>
         </div>
     </div>
   );
